767ac1b3c0e4c025a90abb634a902d735f2f41d7
BuildTools <unconfigured@null.spigotmc.org>
2024-09-07 18:19:40 -0500

    Support for argument parsing

--- encryptor.py
+++ encryptor.py
@@ -1,27 +1,53 @@
 from PIL import Image
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
+import argparse
+import os
 
-# opening Tux
-og_image = Image.open('Tux.jpg')
+def main():
+    
+    # creating parser
+    parser = argparse.ArgumentParser(description="Parsing args for encryptor")
+    parser.add_argument('-i', '--input', type=str, help="Input filename", required=True)
+    parser.add_argument('-o', '--output', type=str, help="Output filename", required=True)
+    parser.add_argument('-k', '--key', type=str, help="Key", required=True)
+    parser.add_argument('-m', '--mode', type=str, help="Encryption mode", required=True)
 
-# turning Tux to bytes
-img_bytes = og_image.tobytes()
+    args = parser.parse_args()
 
-# generating key
-key = b'00112233445566778889aabbccddeeff'
-cipher = AES.new(key, AES.MODE_ECB)
+    # opening Tux
+    if args.input == "Tux.jpg":
+        og_image = Image.open('Tux.jpg')
+    elif args.input == "whoisCB2.jpg":
+        og_image = Image.open('whoisCB2.jpg')
 
-# pad the bytes and then encrypt them with the algorithm
-padded_bytes = pad(img_bytes, AES.block_size)
-encrypted_bytes = cipher.encrypt(padded_bytes)
+    # turning Tux to bytes
+    img_bytes = og_image.tobytes()
 
-# retrieve image size
-width, height = og_image.size
+    # generating key
+    key = args.key.encode('utf-8')
+    # iv for cbc algorithm
+    iv = os.urandom(AES.block_size)
+    
+    if args.mode == "ECB":
+        cipher = AES.new(key, AES.MODE_ECB)
+    elif args.mode == "CBC":
+        cipher = AES.new(key, AES.MODE_CBC, iv)
 
-# change frombytes()
-new_image = Image.frombytes(og_image.mode, (width, height), encrypted_bytes)
+    # pad the bytes and then encrypt them with the algorithm
+    padded_bytes = pad(img_bytes, AES.block_size)
+    encrypted_bytes = cipher.encrypt(padded_bytes)
 
-# show the image
-new_image.save("tux_modded.jpg")
-new_image.show()
+    # retrieve image size
+    width, height = og_image.size
+
+    # change frombytes()
+    new_image = Image.frombytes(og_image.mode, (width, height), encrypted_bytes)
+
+    # show the image
+    new_image.save(args.output)
+    new_image.show()
+    new_image.close()
+
+if __name__ == "__main__":
+    main()
